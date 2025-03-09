55e6075a7a6344ae64f2f47dc386841326d56727
ShubhamPuri-SDET <Spuri4867@gmail.com>
2025-03-10 00:41:19 +0530

    Added 1st test cases for URL Navigation

--- playwrightjava/src/test/java/Testcases/Testcase1.java
+++ playwrightjava/src/test/java/Testcases/Testcase1.java
@@ -8,24 +8,40 @@ import com.microsoft.playwright.Playwright;
 public class Testcase1 {
 
     public static void main(String[] args) {
+        // Create a Playwright instance
         Playwright pw = Playwright.create();
+
+        // Initialize the Chromium browser type
         pw.chromium();
-        // BrowserType bw = pw.webkit();
+
+        // Initialize the browser type to Chromium
         BrowserType bw = pw.chromium();
-        // Browser b = bw.launch();
+        // BrowserType bw = pw.webkit();  // for webkit browser
+        // BrowserType bw = pw.firefox();  // for firefox browser
+
+        // Launch a new browser instance with headless mode set to false
+        // Browser b = bw.launch();  here launching in the headless mode
         Browser b = bw.launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        // Create a new page in the browser
         Page page = b.newPage();
 
+        // Navigate to the specified URL
         page.navigate("https://playwright.dev/java/docs/intro");
 
+        // Get the title of the page
         String title = page.title();
 
+        // Print the page title to the console
         System.out.println("The page title is " + title);
 
+        // Close the page
         page.close();
+
+        // Close the browser
         b.close();
+
+        // Close the Playwright instance
         pw.close();
-
     }
-
 }
